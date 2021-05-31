4866b132808a7d0555cf2cefb39e0aea5d9520e8
A-miin <zulaykaisaeva@gmail.com>
2021-05-31 15:02:22 +0600

    Add serializer

---
+++ Project/api/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+
+from webapp.models import Quote
+
+class QuoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Quote
+        fields = ('id',
+                  'text',
+                  'name',
+                  'email',
+                  'rate',
+                  'is_moderated',
+                  'created_at'
+                  )
+        read_only_fields = ('id', 'created_at')
